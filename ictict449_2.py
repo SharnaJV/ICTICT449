3e66335170aca153465e088d5efa3e31ee427086
LAPTOP-NN8P1NFB\sharn <sharna.verdini@gmail.com>
2023-06-23 15:30:03 +0930

    read comment at top of the code for instructions

---
+++ ictict449_2.py
@@ -0,0 +1,11 @@
+# This code figures out whether x is greater than y. please change the values of x and y
+x=17
+y=17
+if x > y:
+ print("x is greater than y!")  
+ 
+elif x < y:
+        print("x is not greater than y!") 
+
+elif x==y:
+        print("x is the same as y!")
